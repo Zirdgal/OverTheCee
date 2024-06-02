0e6ac1a03b7ac6bae1af32af9cc852299e2627f9
Zirdgal <zirdman@gmail.com>
2024-06-02 23:38:30 +0300

    every single fucking state has data

---
+++ data/stateData.js
@@ -0,0 +1,41 @@
+const ventspilsData = { name: "Ventspils", path: document.getElementById("state-ventspils"), region: "Kurzeme", category: "state" };
+const kuldigaData = { name: "Kuldīga", path: document.getElementById("state-kuldiga"), region: "Kurzeme", category: "state" };
+const liepajaData = { name: "Liepāja", path: document.getElementById("state-liepaja"), region: "Kurzeme", category: "state" };
+const talsiData = { name: "Talsi", path: document.getElementById("state-talsi"), region: "Kurzeme", category: "state" };
+const saldusData = { name: "Saldus", path: document.getElementById("state-saldus"), region: "Kurzeme", category: "state" };
+
+const tukumsData = { name: "Tukums", path: document.getElementById("state-tukums"), region: "Zemgale", category: "state" };
+const dobeleData = { name: "Dobele", path: document.getElementById("state-dobele"), region: "Zemgale", category: "state" };
+const jelgavaData = { name: "Jelgava", path: document.getElementById("state-jelgava"), region: "Zemgale", category: "state" };
+const bauskaData = { name: "Bauska", path: document.getElementById("state-bauska"), region: "Zemgale", category: "state" };
+
+
+const limbaziData = { name: "Limbaži", path: document.getElementById("state-limbazi"), region: "Vidzeme", category: "state" };
+const rigaData = { name: "Rīga", path: document.getElementById("state-riga"), region: "Vidzeme", category: "state" };
+const valmieraData = { name: "Valmiera", path: document.getElementById("state-valmiera"), region: "Vidzeme", category: "state" };
+const cesisData = { name: "Cēsis", path: document.getElementById("state-cesis"), region: "Vidzeme", category: "state" };
+const ogreData = { name: "Ogre", path: document.getElementById("state-ogre"), region: "Vidzeme", category: "state" };
+const valkaData = { name: "Valka", path: document.getElementById("state-valka"), region: "Vidzeme", category: "state" };
+const aluksneData = { name: "Alūksne", path: document.getElementById("state-aluksne"), region: "Vidzeme", category: "state" };
+const gulbeneData = { name: "Gulbene", path: document.getElementById("state-gulbene"), region: "Vidzeme", category: "state" };
+const madonaData = { name: "Madona", path: document.getElementById("state-madona"), region: "Vidzeme", category: "state" };
+
+
+const aizkraukleData = { name: "Aizkraukle", path: document.getElementById("state-aizkraukle"), region: "Sēlija", category: "state" };
+const jekabpilsData = { name: "Jēkabpils", path: document.getElementById("state-jekabpils"), region: "Sēlija", category: "state" };
+const daugavpilsData = { name: "Daugavpils", path: document.getElementById("state-daugavpils"), region: "Sēlija", category: "state" };
+
+const balviData = { name: "Balvi", path: document.getElementById("state-balvi"), region: "Latgale", category: "state" };
+const rezekneData = { name: "Rēzekne", path: document.getElementById("state-rezekne"), region: "Latgale", category: "state" };
+const preiliData = { name: "Preiļi", path: document.getElementById("state-preili"), region: "Latgale", category: "state" };
+const ludzaData = { name: "Ludza", path: document.getElementById("state-ludza"), region: "Latgale", category: "state" };
+const kraslavaData = { name: "Krāslava", path: document.getElementById("state-kraslava"), region: "Latgale", category: "state" };
+
+
+
+
+export const states = [ 
+    ventspilsData, kuldigaData, liepajaData, talsiData, saldusData, tukumsData, dobeleData, jelgavaData, bauskaData, limbaziData,
+    rigaData, valmieraData, cesisData, ogreData, valkaData, aluksneData, gulbeneData, madonaData, aizkraukleData, jekabpilsData,
+    daugavpilsData, balviData, rezekneData, preiliData, ludzaData, kraslavaData
+ ];
