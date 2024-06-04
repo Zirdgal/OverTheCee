f761211ba4c9024a4554dcd8e3aacff9376753cb
Zirdgal <zirdman@gmail.com>
2024-06-04 15:55:43 +0300

    added functionality to recruit divisions

--- scripts/card-handler-script.js
+++ scripts/card-handler-script.js
@@ -1,4 +1,5 @@
 import { resourceCount } from "./resource-script.js";
+import { updateAllUnitImages } from "./unit-handler-script.js";
 import { cards } from "../data/cardData.js"
 import { states } from "../data/stateData.js";
 
@@ -12,35 +13,96 @@ const modalText = document.getElementById("modal-text");
 
 // Selected State Actions
 const selectedStateActionButton1 = document.getElementById("selected-state-action-button-1");
+const selectedStateActionButton2 = document.getElementById("selected-state-action-button-2");
+
+
+function resetModalStyle() {
+    modal.style.pointerEvents = "auto";
+    modal.style.backgroundColor = "rgba(0,0,0,0.4)";
+    modalContent.style.marginTop = "25%";
+    modalText.innerHTML = "Please select an option:"
+    modal.style.display = "block";
+    modalBtn1.style.display = "inline-block";
+    modalBtn2.style.display = "inline-block";
+    // Sets all the modal styles back to default!
+    return;
+};
+function stateSelectionModalStyle() {
+    modal.style.backgroundColor = "transparent";
+    modal.style.pointerEvents = "none";
+    modalContent.style.marginTop = "5%";
+    modalBtn1.style.display = "none";
+    modalBtn2.style.display = "none";
+    modalText.innerHTML = "Please select a state..."
+    // Change the Modals to allow you to click on the state
+    return;
+};
+
+
+
+
+
 
 document.addEventListener("DOMContentLoaded", function() {
 
     cards.forEach(card => {
         card.path.addEventListener("click", function() {
-            if (card.cost >= resourceCount) {
-                modal.style.display = "block"; // Bassically look at EVEYR SINGLE Card you got and then when clicked show modal (options page)
+            if (card.cost >= resourceCount) {// Bassically look at EVEYR SINGLE Card you got and then when clicked show modal (options page)
+                resetModalStyle() // Sets all the modal styles back to default by calling a function!
             };
         });
     });
 
 
     modalBtn1.onclick = function() {
-        modal.style.display = "none";
+        modal.style.display = "none"; // TESTING MODE DOES NOTHING AS OF CURRENT
     };
-    modalBtn2.onclick = function() {
-        modal.style.backgroundColor = "transparent";
-        modal.style.pointerEvents = "none";
-        modalContent.style.marginTop = "5%";
-        modalBtn1.style.display = "none";
-        modalBtn2.style.display = "none";
-        modalText.innerHTML = "Please select a state..."
+
+
+    modalBtn2.onclick = function() { // if the option 2 is clicked
+
+        let isCardUsed = false; // this is here so the eventlisteners dont screw me over
+
+        stateSelectionModalStyle() // Change the Modals to allow you to click on the state
 
         states.forEach(state => {
             state.path.addEventListener("click", function() {
-                if (state.ownedBy = "lat") {
-                    selectedStateActionButton1.style.display = "block";
+                if (state.ownedBy === "lat" && isCardUsed === false) {
+                    selectedStateActionButton1.disabled = false;
+                    selectedStateActionButton1.style.display = "inline-block";
                     selectedStateActionButton1.innerHTML = "Recruit 1 Latvian Division";
-                }
+
+                    selectedStateActionButton1.onclick = function() {
+                        if (state.ownedBy === "lat") {
+                            isCardUsed = true;
+                            state.latUnits++;
+                            updateAllUnitImages();
+
+                            selectedStateActionButton1.style.display = "none";
+                            selectedStateActionButton2.style.display = "none";
+                            modal.style.display = "none";
+                        };
+                    }
+
+                    selectedStateActionButton2.disabled = false;
+                    selectedStateActionButton2.style.display = "inline-block";
+                    selectedStateActionButton2.innerHTML = "Recruit 2 German Divisions";
+
+                    selectedStateActionButton2.onclick = function() {
+                        if (state.ownedBy === "lat") {
+                            isCardUsed = true;
+                            state.gerUnits += 2;
+                            updateAllUnitImages();
+
+                            selectedStateActionButton1.style.display = "none";
+                            selectedStateActionButton2.style.display = "none";
+                            modal.style.display = "none";
+                        };
+                    }
+                } else {
+                    selectedStateActionButton1.disabled = true;
+                    selectedStateActionButton2.disabled = true;
+                };
             });
         });
     };
