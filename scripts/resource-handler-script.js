18a68aedcc0e4a02c02d2fa991a8b8bf4de0c1c4
Zirdgal <zirdman@gmail.com>
2024-06-07 22:31:57 +0300

    added documentation for the resource handler script

--- scripts/resource-handler-script.js
+++ scripts/resource-handler-script.js
@@ -1,20 +1,20 @@
-export let resourceCount = 2;
+export let resourceCount = 2; // your current resources are saved here
 
 const resourceCountObj = document.getElementById("resouce-counter");
-
+// the object in the top right
 
 function updateResources() {
-    resourceCountObj.innerHTML = `Your resource count: ${resourceCount}`;
+    resourceCountObj.innerHTML = `Your resource count: ${resourceCount}`; // changes the counter in the top right
     return;
 };
 
-export function addResources(addedResources) {
-    resourceCount += Number(addedResources);
-    updateResources();
+export function addResources(addedResources) { // this function gets called in card-handler-script.js
+    resourceCount += Number(addedResources); // changes tehc current resources to the number version of the given addedResources variable
+    updateResources(); // call the function up above
     return;
 }
 export function subtractResources(subtractedResources) {
-    resourceCount -= Number(subtractedResources);
-    updateResources();
+    resourceCount -= Number(subtractedResources); // changes tehc current resources to the number version of the given subtractedResources variable
+    updateResources(); // call the function up above
     return;
 }
