cf36af0a5e24c512d6ac5cca10438b6ea3dca0a6
Zirdgal <zirdman@gmail.com>
2024-06-09 19:47:20 +0300

    new card!

--- data/cardData.js
+++ data/cardData.js
@@ -1,7 +1,8 @@
 const marchData = { name: "March", path: document.getElementById("march-card"), cost: "0", option1: "March", option1Level: "2", option2: "Recruit", option2Level: "2" };
+const recruitData = { name: "Recruit", path: document.getElementById("recruit-card"), cost: "0", option1: "Recruit", option1Level: "2", option2: "Resource", option2Level: "2" };
 const karlisUlmanisData = { name: "Kārlis Ulmanis", path: document.getElementById("karlis-ulmanis-card"), cost: "2", option1: "Resource", option1Level: "5", option2: "none", option2Level: "0" };
 
 
 
 
-export const cards = [ marchData, karlisUlmanisData ];
+export const cards = [ marchData, karlisUlmanisData, recruitData ];
