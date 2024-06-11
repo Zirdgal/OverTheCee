2d15a9a8cc5ca3af6ca614c8d369db808ee63c0a
Zirdgal <zirdman@gmail.com>
2024-06-12 00:26:05 +0300

    added a script that replenishes your cards after your turn ends

--- scripts/end-turn-script.js
+++ scripts/end-turn-script.js
@@ -1,11 +1,71 @@
-import { cards } from "../data/cardData.js";
+import { cards, enemyCards } from "../data/cardData.js";
+import { states } from "../data/stateData.js";
 import { updateCardStates } from "./card-handler-script.js";
+import { updateAllUnitImages } from "./unit-handler-script.js";
+import { subtractResources } from "./resource-handler-script.js";
 
 const endTurnButton = document.getElementById("end-turn-button");
 
+// modal elements
+const modal = document.getElementById("modal-container");
+const modalContent = document.getElementById("modal-content");
+const modalBtn1 = document.getElementById("modal-btn-1");
+const modalBtn2 = document.getElementById("modal-btn-2");
+const modalText = document.getElementById("modal-text");
+const enemyCardImage = document.getElementById("enemy-card-img");
+
 endTurnButton.onclick = function endTurn() {
-    cards.forEach(card => {
-        card.used = false;
-        updateCardStates();
+
+    console.log("end turn button clicked");
+
+    modal.style.pointerEvents = "auto";
+    modal.style.backgroundColor = "rgba(0,0,0,0.2)";
+    modalContent.style.marginTop = "10vh";
+    modalText.innerHTML = "The Soviets Turn:"
+    modal.style.display = "block";
+    modalBtn1.style.display = "none";
+    modalBtn2.style.display = "none";
+
+    const updatedStates = new Set();
+
+    states.forEach(state => {
+        if (state.ownedBy === "lat") {
+            const adjacentStates = [
+                state.adjacentState1, state.adjacentState2, state.adjacentState3,
+                state.adjacentState4, state.adjacentState5, state.adjacentState6,
+                state.adjacentState7, state.adjacentState8
+            ].filter(Boolean);
+
+            adjacentStates.forEach(adjacentStateName => {
+                const adjacentState = states.find(st => st.name === adjacentStateName);
+                if (adjacentState && adjacentState.ownedBy === "sov" && !updatedStates.has(adjacentState.name)) {
+                    adjacentState.sovUnits = Math.min(adjacentState.sovUnits + 2, 6);
+                    updatedStates.add(adjacentState.name);
+                };
+            });
+        };
+    });
+
+    updateAllUnitImages();
+
+    enemyCards.forEach(card => {
+        enemyCardImage.style.display = "block";
+
+        if (card.name === "Placeholder") {
+            subtractResources(2);
+        }
     })
-}
+
+    setTimeout(continueGame, 2500);
+
+    function continueGame() {
+        console.log("continueing game?");
+        modal.style.display = "none";
+        modal.style.pointerEvents = "none";
+
+        cards.forEach(card => {
+            card.used = false;
+            updateCardStates();
+        });
+    };
+};
