2adc00159ff67c12438babaa71fa5ab0114e86f9
Zirdgal <zirdman@gmail.com>
2024-06-12 05:12:58 +0300

    card data

--- data/cardData.js
+++ data/cardData.js
@@ -1,11 +1,12 @@
 const marchData = { name: "March", used: false, path: document.getElementById("march-card"), cost: "0", option1: "March", option1Level: "2", option2: "Recruit", option2Level: "2" };
 const recruitData = { name: "Recruit", used: false, path: document.getElementById("recruit-card"), cost: "0", option1: "Recruit", option1Level: "2", option2: "Resource", option2Level: "2" };
 const karlisUlmanisData = { name: "Kārlis Ulmanis", used: false, path: document.getElementById("karlis-ulmanis-card"), cost: "2", option1: "Resource", option1Level: "5", option2: "none", option2Level: "0" };
+const m1890Data = { name: "M1890", used: false, path: document.getElementById("m1890-card"), cost: "0", option1: "March", option1Level: "2", option2: "none", option2Level: "0" };
 
 
 
 
-export const cards = [ marchData, karlisUlmanisData, recruitData ];
+export const cards = [ marchData, karlisUlmanisData, recruitData, m1890Data ];
 
 
 
