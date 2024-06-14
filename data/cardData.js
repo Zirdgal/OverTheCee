66893ab99fb6bf8697bc9e2b0e213d8aa17a5468
Zirdgal <zirdman@gmail.com>
2024-06-14 10:07:48 +0300

    added the new card to html and data

--- data/cardData.js
+++ data/cardData.js
@@ -4,11 +4,12 @@ const karlisUlmanisData = { name: "Kārlis Ulmanis", used: false, path: document
 const m1890Data = { name: "M-1890", used: false, path: document.getElementById("m1890-card"), cost: "0", option1: "March", option1Level: "1", option2: "none", option2Level: "0" };
 const m1902Data = { name: "M-1902", used: false, path: document.getElementById("m1902-card"), cost: "1", option1: "March", option1Level: "2", option2: "none", option2Level: "0" };
 const m1910Data = { name: "M-1910", used: false, path: document.getElementById("m1910-card"), cost: "2", option1: "March", option1Level: "3", option2: "none", option2Level: "0" };
+const alliedShipsData = { name: "English and French War Fleet", used: false, path: document.getElementById("allied-ships-card"), cost: "1", option1: "Resource", option1Level: "2", option2: "alliedShips", option2Level: "1" };
 
 
 
 
-export const cards = [ marchData, karlisUlmanisData, recruitData, m1890Data, m1902Data, m1910Data ];
+export const cards = [ marchData, karlisUlmanisData, recruitData, m1890Data, m1902Data, m1910Data, alliedShipsData ];
 
 
 
