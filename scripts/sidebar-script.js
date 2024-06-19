1988cd88f12177f2ce040886d33004e4e8d0568b
Zirdgal <zirdman@gmail.com>
2024-06-19 09:31:58 +0300

    if you click on a country you now get data about it

--- scripts/sidebar-script.js
+++ scripts/sidebar-script.js
@@ -4,27 +4,23 @@ const btn = document.getElementById("sidebar-button");
 const sideBarContent = document.getElementById("sidebar-full-container");
 const endTurnButton = document.getElementById("end-turn-button");
 
-let sideBarState = false;
+let sideBarState = false; // variable to just keep track if the sidebar is open
 
-btn.onclick = function moveSideBar() {
-    if (sideBarState === true) {
-        sideBarContent.style.display = "none";
-        btn.innerHTML = ">";
-        sideBarState = false;
-        endTurnButton.style.display = "block";
-    } else {
-        sideBarContent.style.display = "block";
-        btn.innerHTML = "<";
-        sideBarState = true;
-        endTurnButton.style.display = "block";
+btn.onclick = function moveSideBar() { // if the sidebar button is clicked ...
+    if (sideBarState === true) { // AND if the sidebar is open...
+        sideBarContent.style.display = "none"; // close the bar
+        btn.innerHTML = ">"; // make the sidebar arrow turn around
+        sideBarState = false; // make the variable say that the sidebar is closed
+    } else { // Or if the sidebar is closed....
+        sideBarContent.style.display = "block"; // open the side bar
+        btn.innerHTML = "<"; // turn that arrow!
+        sideBarState = true; // update the variable 
+        endTurnButton.style.display = "block"; // (this is for when the player opens the game for the first time)
     }
 };
-document.body.onkeyup = function(e) {
-    if (e.key == " " ||
-        e.code == "Space" ||      
-        e.keyCode == 32      
-    ) {
-        if (sideBarState === true) {
+document.body.onkeyup = function(e) { // when any key is pressed
+    if ( e.key == " " || e.code == "Space" || e.keyCode == 32 ) { // if the key is the spacebar do the same as the above lmfao
+        if (sideBarState === true) { 
             sideBarContent.style.display = "none";
             btn.innerHTML = ">";
             sideBarState = false;
@@ -40,22 +36,28 @@ document.body.onkeyup = function(e) {
 
 // State Selection
 
-const selectedStateElem = document.getElementById("selected-state-text");
-const selectedStateRegion = document.getElementById("selected-state-region");
-import { states } from "../data/stateData.js";
+const selectedStateElem = document.getElementById("selected-state-text"); // the <h1> tag
+const selectedStateRegion = document.getElementById("selected-state-region"); // the <p> tag
+import { statesAndCountries } from "../data/stateData.js"; // get all of the state data :3
 
 function updateSelectedStateUI(state) {
     selectedStateElem.innerHTML = state.name;
     selectedStateRegion.innerHTML = state.region;
     if (state.ownedBy === "sov") {
         selectedStateElem.style.color = "red";
+    } else if (state.ownedBy === "lit"){
+        selectedStateElem.style.color = "yellow";
+    } else if (state.ownedBy === "ger"){
+        selectedStateElem.style.color = "gray";
+    } else if (state.ownedBy === "est"){
+        selectedStateElem.style.color = "blue";
     } else {
         selectedStateElem.style.color = "green";
     }
 }
 
 function attachStateClickListeners() {
-    states.forEach(state => {
+    statesAndCountries.forEach(state => {
         state.path.addEventListener("click", function() {
             updateSelectedStateUI(state); // Update selected state UI
         });
