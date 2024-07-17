0ff6aa59aead8e37c0bab4e23b07ab8a5e1c61ea
Zirdgal <zirdman@gmail.com>
2024-07-17 10:22:39 +0300

    New Equipment Card

--- data/cardData.js
+++ data/cardData.js
@@ -1,5 +1,6 @@
 const marchData = { name: "March", id: "march", used: false, path: document.getElementById("march-card"), cost: "0", option1: "March", option1Level: "2", option2: "Recruit", option2Level: "2" };
 const recruitData = { name: "Recruit", id: "recruit" , used: false, path: document.getElementById("recruit-card"), cost: "0", option1: "Recruit", option1Level: "2", option2: "Resource", option2Level: "2" };
+const equipmentData = { name: "Equipment", id: "equipment" , used: false, path: document.getElementById("equipment-card"), cost: "0", option1: "Resource", option1Level: "2", option2: "March", option2Level: "2" };
 const karlisUlmanisData = { name: "Kārlis Ulmanis", id: "karlisUlmanis", used: false, path: document.getElementById("karlis-ulmanis-card"), cost: "2", option1: "Resource", option1Level: "5", option2: "none", option2Level: "0" };
 const m1890Data = { name: "M-1890", id: "m1890", used: false, path: document.getElementById("m1890-card"), cost: "0", option1: "March", option1Level: "1", option2: "none", option2Level: "0" };
 const m1902Data = { name: "M-1902", id: "m1902", used: false, path: document.getElementById("m1902-card"), cost: "1", option1: "March", option1Level: "2", option2: "none", option2Level: "0" };
@@ -9,7 +10,7 @@ const alliedShipsData = { name: "English and French War Fleet", id: "alliedShips
 
 
 
-export const cards = [ marchData, karlisUlmanisData, recruitData, m1890Data, m1902Data, m1910Data, alliedShipsData ];
+export const cards = [ marchData, karlisUlmanisData, recruitData, m1890Data, m1902Data, m1910Data, alliedShipsData, equipmentData ];
 
 
 
