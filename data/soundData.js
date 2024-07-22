d410503f1208caf86183f494457fbff93bb11d5b
Zirdgal <zirdman@gmail.com>
2024-07-22 16:14:55 +0300

    all new sound effects

---
+++ data/soundData.js
@@ -0,0 +1,8 @@
+﻿export const storeSFX = document.getElementById("store-audio");
+export const clickSFX = document.getElementById("click-audio");
+export const chaChingSFX = document.getElementById("cha-ching-audio");
+
+clickSFX.volume = 0.05;
+storeSFX.volume = 0.4;
+chaChingSFX.volume = 0.25;
+
